e754165098ec6ed6495de6847b069de1b4c671cc
Jorge Carrera <Jorge Carrera>
2017-04-21 00:37:51 -0700

    NRZI implemented on incoming bytes. Still working on 4B/5B.

--- PhysLayerClient.java
+++ PhysLayerClient.java
@@ -11,9 +11,11 @@ import java.net.Socket;
 public class PhysLayerClient {
         public static void main(String[] args) throws Exception {
                     try (Socket socket = new Socket("codebank.xyz", 38002)) {
-                    byte a = 0;
-                    byte[] b = new byte[32];
+                    byte a;
+                    byte[] b = new byte[320];
+                    byte[] converted = new byte[320];
                     float c = 0.0f;
+                    int previousNum = 0;
                         
                     InputStream is = socket.getInputStream();
                     OutputStream os = socket.getOutputStream();
@@ -30,7 +32,7 @@ public class PhysLayerClient {
                     System.out.printf("%.2f" + "\n", c);
                     
                     System.out.print("Bytes recieved: " + "\n");
-                    for(int j = 0; j < 32; j++){
+                    for(int j = 0; j < 320; j++){
                         a = (byte) is.read();
                         b[j] = a;
                         String s1 = String.format("%8s", Integer.toBinaryString(b[j] & 0xFF)).replace(' ', '0');
@@ -40,14 +42,33 @@ public class PhysLayerClient {
                     System.out.println();
                     
                     System.out.print("Bytes translat: ");
-                    for(int k = 0; k < 32; k++){
-                        if(b[k] < c ){
-                            System.out.print("0  ");
-                        }else{
-                            System.out.print("1  ");
+                    for(int k = 0; k < 320; k++){
+                        for(int l = 7; l > 0; l--){
+                            if(previousNum == 0){
+                                if((((b[l] >> l) & 1) == 1)){
+                                    b[l] |= (1 << l);
+                                    previousNum = 1;
+                                }
+                            }else{
+                                if((((b[l] >> l) & 1) == 1)){
+                                    b[l] |= (0 << l);
+                                    previousNum = 1;
+                                }
+                            }
                         }
                     }
+                    
                     System.out.println();
+                    
+                    for(int m = 0; m < 320; m++){
+                        a = (byte)00000000;
+                        if(b[m] > c){
+                        }
+                    }
+                    
+                    System.out.print("Good? ");
+                    os.write(b);
+                    System.out.print(is.read());
                     //System.out.print(String.format("%x", a) + " ");
                     }
         }
